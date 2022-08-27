3a40a3bccbb7298b18a7b27f00d1428345f4cd99
Nachoo Perrotta <nachoperro26@gmail.com>
2022-08-27 13:13:41 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ItemListConteiner from './components/ItemListConteiner';
+import NavBar from './components/NavBar'; 
 
-function App() {
+function App (props){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ignacio Perrotta
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavBar/>
+      <ItemListConteiner greeting={"4"}/>
     </div>
   );
 }
 
 export default App;
+
